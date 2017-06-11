22b854082293fb7eafec0f04052bda4d764afd2e
Atilla <atillanegreirosmaia@gmail.com>
2017-06-11 11:43:44 -0300

    Modifications

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,8 @@
+import bcrypt
 from app import app
-from bottle import template, static_file
-from bottle import request
+from app.models.tables import User
+from bottle import template, static_file, request, redirect
+
 # static routes
 @app.get('/<filename:re:.*\.css>')
 def stylesheet(filename):
@@ -14,42 +16,56 @@ def javascripts(filename):
 def images(filename):
 	return static_file(filename, root='app/static/img')
 
-@app.route('/') # @get('/login')
-def login():
-	return template('login')
-
-def check_login(email, password):
-	d = {'atilla@gmail.com':'python', 'jobs@gmail.com':'apple', 'gates@gmail.com':'microsoft'}
-	if email in d.keys() and d[email] == password:
-		return True
-	return False
-
-@app.route('/', method='POST') # @post('/login')
-def do_login():
-	email = request.forms.get('email')
-	password = request.forms.get('password')
-	return template('verify_login', sucess=check_login(email, password))
-
 @app.route('/register')
 def do_register():
-	return template('register')
-
-def check_register(email, password, confirm_password):
-	d = {'atilla':'atilla@gmail.com', 'jobs':'jobs@gmail.com', 'gates':'gates@gmail.com'}
-	if email in d.values():
-		return 0
-	elif password != confirm_password:
-		return -1
-	else:
-		return 1
+	return template('register', username_exists=False, email_exists=False)
 
 @app.route('/register', method='POST')
-def do_register():
+def do_register(db, session):
 	username = request.forms.get('username')
 	email = request.forms.get('email')
 	password = request.forms.get('password')
 	confirm_password = request.forms.get('confirm-password')
-	return template('verify_register', sucess=check_register(email, password, confirm_password))
+	
+	if db.query(User).filter(User.email==email).one_or_none():
+		return template('register', email_exists=True, username_exists=False)
+
+	if db.query(User).filter(User.username==username).one_or_none():
+		return template('register', email_exists=False, username_exists=True)
+
+	password = password.encode()
+	hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+	new_user = User(username, email, hashed)
+	db.add(new_user)
+	session['name'] = email
+	return redirect('home')
+
+@app.route('/') # @get('/login')
+def login():
+	return template('login', email_ok=True, password_ok=True)
+
+@app.route('/', method='POST') # @post('/login')
+def do_login(db, session):
+	email = request.forms.get('email')
+	password = request.forms.get('password')
+
+	try:
+		user = db.query(User).filter(User.email==email).one()
+	except:
+		return template('login', email_ok=False, password_ok=True)
+
+	if bcrypt.checkpw(password.encode(), user.password):
+		session['name'] = email
+		return redirect('home')
+	else:
+		return template('login', email_ok=True, password_ok=False, email=email)
+
+@app.route('/home')
+def do_register(db, session):
+	if not session.get('name'):
+		return template('login', email_ok=True, password_ok=True)
+	return template('home')
 
 @app.error(404)
 def error404(error):
