b0c3d44601ca23af56b5c9b73ea9decf38431c3b
Atilla <atillanegreirosmaia@gmail.com>
2017-06-12 10:01:50 -0300

    Login

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,11 +1,3 @@
-$(document).ready(function(){
-  $(document).mousemove(function(e){
-     TweenLite.to($('body'), 
-        .5, 
-        { css: 
-            {
-                backgroundPosition: ""+ parseInt(event.pageX/8) + "px "+parseInt(event.pageY/'12')+"px, "+parseInt(event.pageX/'15')+"px "+parseInt(event.pageY/'15')+"px, "+parseInt(event.pageX/'30')+"px "+parseInt(event.pageY/'30')+"px"
-            }
-        });
-  });
-});
+function submit() {
+	document.getElemantById("form1").submit();
+}
